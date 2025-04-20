05e8d8e193004c5848a6dabd33d1ff23322cef2f
Chinnam Vamsee Krishna <chinnamvamseekrishna@gmail.com>
2025-04-20 09:39:58 +0530

    render deploy

--- backend/index.js
+++ backend/index.js
@@ -6,65 +6,12 @@ const movieRouter = require("./routes/movieRoute");
 const theatreRouter = require("./routes/theatreRoute");
 const showRouter = require("./routes/showRoute");
 const bookingRouter = require("./routes/bookingRoute");
-const bodyParser = require("body-parser");
-const bookingModel = require("./models/bookingModel");
-const showModel = require("./models/showModel");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
-const secret = process.env.WEB_HOOK_SECRET;
+const path = require("path");
+
 const app = express();
+const clientBuildPath = path.join(__dirname, "../frontend/build");
 
-app.post(
-  "/webhook",
-  bodyParser.raw({ type: "application/json" }),
-  async (req, res) => {
-    const sig = req.headers["stripe-signature"];
-    let event;
-
-    try {
-      event = stripe.webhooks.constructEvent(req.body, sig, secret);
-    } catch (err) {
-      console.log(`Webhook signature verification failed:`, err.message);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    // Handle the event
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-
-      const userId = session.metadata.userId;
-      const showId = session.metadata.showId;
-      const seats = session.metadata.seats.split(";");
-
-      try {
-        const booking = new bookingModel({
-          user: userId,
-          show: showId,
-          seats: seats,
-          transactionId: session.id,
-        });
-        await booking.save();
-
-        const show = await showModel.findById(showId).populate("movie");
-        const updatedBookedSeats = [...show.bookedSeats, ...seats];
-        await showModel.findByIdAndUpdate(showId, {
-          bookedSeats: updatedBookedSeats,
-        });
-        res.status(200).json({
-          success: true,
-          message: "Booking successful",
-          data: booking,
-        });
-      } catch (error) {
-        console.error("Booking error:", error);
-        res.status(500).json({
-          success: false,
-          message: "Booking failed",
-        });
-      }
-    }
-    console.log("Webhook received");
-  }
-);
+app.use(express.static(clientBuildPath));
 
 app.use(express.json());
 connectDB();
@@ -75,6 +22,10 @@ app.use("/api/movies", movieRouter);
 app.use("/api/theatres", theatreRouter);
 app.use("/api/shows", showRouter);
 app.use("/api/booking", bookingRouter);
+
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+});
 
 app.listen(8082, () => {
   console.log("Server is running on port 8082");
